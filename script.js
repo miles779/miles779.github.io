399e6661d74dd65690f7baf2fb65b89c42a72094
miles779 <miles@omans.org>
2026-03-07 22:03:22 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
-let count = 0;
-
-document.getElementById("click-btn").addEventListener("click", () => {
-  count++;
-  document.getElementById("counter-text").textContent =
-    `You've clicked ${count} times`;
+document.getElementById("hello-btn").addEventListener("click", () => {
+  document.getElementById("hello-text").textContent =
+    "Hello from MilesLab!";
 });
+
